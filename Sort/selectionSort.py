353dd55523670bb2283c78e15ed19264e61bcf12
Journey <g48993818@gmail.com>
2021-09-14 22:04:20 +0900

    sort

---
+++ Sort/selectionSort.py
@@ -0,0 +1,67 @@
+def selectionSort(a, n):
+    
+    for i in range(1, n):
+        
+        minindex = i
+        
+        for j in range(i+1, n+1):
+            
+            if a[j] < a[minindex]:
+                
+                minindex = j
+                
+        a[i], a[minindex] = a[minindex], a[i]
+    return a
+        
+
+
+
+
+def checkSort(a, n):
+
+    isSorted = True
+
+    for i in range(1, n):
+
+        if a[i] > a[i+1]:
+
+            isSorted = False
+
+        if not isSorted:
+
+            break
+
+    if isSorted:
+
+        print("정렬 완료")
+
+    else:
+
+        print("정렬 오류 발생")
+
+
+
+import random, time
+
+
+
+N = 15000
+
+a = []
+
+a.append(None)
+
+for i in range(N):
+
+    a.append(random.randint(1, N))
+
+start_time = time.time()
+
+selectionSort(a, N)
+
+end_time = time.time() - start_time
+
+print("선택 정렬의 실행 시간 (N=%d) : %0.3f"%(N, end_time))
+
+checkSort(a, N)
+
